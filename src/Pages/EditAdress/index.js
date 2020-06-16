b4c81786de11049b7df96fca6d1ac84712c07538
Bernardo Arduini <berzin-@hotmail.com>
2020-06-16 10:33:37 -0300

    subindo Edit Adress

--- src/Pages/EditAdress/index.js
+++ src/Pages/EditAdress/index.js
@@ -1,12 +1,106 @@
 import React from 'react';
-import Styled from 'styled-components'
+import { Container, Form, ButtonStyled, Header, ArrowBackIosStyled, Label } from'./style' 
+import { TextField } from '@material-ui/core'
+import { useForm } from '../../hooks/useForm'
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme, useStyles } from '../../Components/MaterialTheme/theme'
 
-  
+
 function EditAdress() {
+  const classes = useStyles()
+  const [form, onChangeInput] = useForm({
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    cidade: '',
+    estado: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div >
-    
-    </div>
+    <Container>
+      <Header>
+        <ArrowBackIosStyled />
+      </Header>
+      <Form onSubmit={handleSubmit}>
+        <ThemeProvider theme={theme}>
+          <TextField
+            required
+            type={'text'}
+            placeholder={'Rua/ Av.'}
+            name={'name'}
+            label="Logradouro"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <TextField
+            required
+            type={'number'}
+            placeholder={'Numero.'}
+            name={'name'}
+            label="Numero"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <TextField
+            required
+            type={'text'}
+            placeholder={'Complemento.'}
+            name={'name'}
+            label="Complemento"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <TextField
+            required
+            type={'text'}
+            placeholder={'Bairro.'}
+            name={'name'}
+            label="Bairro"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <TextField
+            required
+            type={'text'}
+            placeholder={'Cidade.'}
+            name={'name'}
+            label="Cidade"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <TextField
+            required
+            type={'text'}
+            placeholder={'Estado.'}
+            name={'name'}
+            label="Estado"
+            value={form.name}
+            variant="outlined"
+            onChange={onChangeInput}
+          />
+          <ButtonStyled
+            classes={{
+              root: classes.root,
+              label: classes.label
+            }}
+            type={'submit'}
+            variant={'contained'}
+            color={'primary'}>
+            Salvar
+        </ButtonStyled>
+        </ThemeProvider>
+      </Form>
+    </Container>
   );
 }
 
